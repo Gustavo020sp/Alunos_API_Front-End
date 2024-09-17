54d55728af1e130fd52502d9bc74d8835ddbfad6
Gustavo Gomes <gustavo020sp@gmail.com>
2024-09-17 17:10:39 -0300

    initial commit

---
+++ src/pages/Alunos/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import './styles.css';
+import logoCadastro from '../../assets/cadastro.png';
+import { FiUserX, FiXCircle, FiEdit } from "react-icons/fi";
+import api from '../../services/api';
+
+export default function Alunos() {
+
+    const [nome, setNome] = useState('');
+    const [alunos, setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const navigate = useNavigate();
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+
+
+    useEffect(() => {
+        api.get('api/alunos', authorization).then(
+            response => {
+                setAlunos(response.data);
+            }
+        ).catch(error => {
+            console.error('Erro ao buscar alunos:', error);
+        });
+    }, [token]); // Adicionei o array de dependências com o token
+
+
+    async function logout(){
+        try{
+            localStorage.clear();
+            localStorage.setItem("token", '');
+            authorization.headers = '';
+            navigate('/');
+        }
+        catch(err){
+            alert('Não foi possível fazer o logout ' + err);
+        }
+    }
+
+    return (
+        <div className="aluno-container">
+            <header>
+                <img src={logoCadastro} alt='Cadastro' />
+                <span>Bem vindo, <strong>{email}</strong></span>
+                <Link className="button" to="/aluno/novo/0">Novo Aluno</Link>
+
+                <button onClick={logout} type="button">
+                    <FiXCircle size={35} color="#17202a" />
+                </button>
+            </header>
+            <form>
+                <input type='text' placeholder='Nome' />
+                <button type="button" class='button'>
+                    Filtrar aluno por nome (parcial)
+                </button>
+            </form>
+            <h1>Relação de Alunos</h1>
+            <ul>
+                {alunos.map(aluno => (
+                    <li key={aluno.id}>
+                        <b>Nome:</b>{aluno.nome}<br /><br />
+                        <b>Email:</b>{aluno.email}<br /><br />
+                        <b>Idade:</b>{aluno.idade}<br /><br />
+                        <button type="button">
+                            <FiEdit size="25" color="#17202a" />
+                        </button>
+
+                        <button type="button">
+                            <FiUserX size="25" color="#17202a" />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
